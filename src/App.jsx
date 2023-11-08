1c8eb085a0ed7caca5f4cb619cab3f69e77a7107
Sydnee Snowball <s.snowball@hotmail.com>
2023-11-08 14:53:41 -0800

    Some shopping cart changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,57 @@ import './App.css'
 
 function App() {
 
-const products = [
-  "Turkey Bacon",
-  "Eggs",
-  "Yogurt",
-  "Coffee",
-  "Almond Milk",
-  "Bagels",
-  "Apples"
-];
+  const products = [
+    "Turkey Bacon",
+    "Eggs",
+    "Yogurt",
+    "Coffee",
+    "Almond Milk",
+    "Bagels",
+    "Apples"
+  ];
 
-const selectProducts = (listProducts) => 
-<select>{
-    listProducts.map((item, index) =>
-    <option key={index}>{item}</option>
-    )}</select>
+  const userCart = [];
+
+  const [cart, setCart] = useState(userCart);
+
+  const count = 0;
+
+  const [itemCount, setCount] = useState(count);
+
+  const selectProducts = (listProducts) => 
+  <select>{
+      listProducts.map((item, index) =>
+      <option key={index}>{item}</option>
+      )}</select>;
+
+  // const selectProducts = (listProducts) => {
+  //   const items = listProducts.map((item, index) => {
+  //     return <li key={index}>{item}</li>;
+  //   });
+  //   return <ul>{items}</ul>
+  // }
+
+{/* {selectProducts(products)}<input>{selectProducts(products)}</input> 
+<select><input>{selectProducts(products)}</input></select>
+        <input><select><option>{selectProducts(products)}</option></select></input>
+      <div className='cart-items'>
+        <ul>
+          <li>{products[index]}</li>
+        </ul>
+      </div>*/}
 
   return (
     <>
       <h1>Shopping Cart</h1>
-      <section>
-      <h3>Products: </h3> 
-        <form>
-          {selectProducts(products)}
-          <button type="submit">Add To Cart</button>
-        </form>
-      </section>
+      <form>
+        <label htmlFor=''>Products:</label>
+        {selectProducts(products)}
+        <button type="submit">Add To Cart</button>
+      </form>
+      <p>
+        {itemCount === 0 ? "Add Some Items" : "You have " + {itemCount} +  " item(s) in your cart"}
+      </p>
     </>
   )
 }
