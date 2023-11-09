f35567cc3fa4ecdd412ce7b2ac5d000b5dfae66f
Sydnee Snowball <s.snowball@hotmail.com>
2023-11-09 10:31:01 -0800

    most recent changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,47 @@ function App() {
     "Apples"
   ];
 
-  const userCart = [];
+  // const userCart = [];
 
-  const [cart, setCart] = useState(userCart);
+  const [cart, setCart] = useState([]);
 
-  const count = 0;
+  // const count = 0;
 
-  const [itemCount, setCount] = useState(count);
+  const [itemCount, setItemCount] = useState(0);
 
   const selectProducts = (listProducts) => 
-  <select>{
+    <select>{
       listProducts.map((item, index) =>
       <option key={index}>{item}</option>
-      )}</select>;
+         )}</select>;
 
+  const addItems = (event) => {
+    event.preventDefault();
+    const item = event.target.elements.products.value;
+    // if(cart.includes(item)) {
+    //   const items = { ... itemCount};
+    //   items.itemCount += 1;
+    //   setItemCount(items);
+    // }
+    // else {
+    //   setCart([ ... cart, item])
+    // }
+    if(cart.includes(item)){
+      setCart(item.quantity += 1);
+      let moreItems = { ... itemCount};
+      moreItems += 1;
+      setItemCount(moreItems);
+    }
+    else {
+      const newItem = {
+        itemName: item,
+        quantity: 1
+      };
+      setCart([ ... cart, newItem]);
+    }
+
+  }
+      
   // const selectProducts = (listProducts) => {
   //   const items = listProducts.map((item, index) => {
   //     return <li key={index}>{item}</li>;
@@ -41,18 +68,25 @@ function App() {
         <ul>
           <li>{products[index]}</li>
         </ul>
-      </div>*/}
+      </div>
+    onClick={addItems(event)}
+  <input id='select-product'>{selectProducts(products)}</input>*/}
 
   return (
     <>
       <h1>Shopping Cart</h1>
       <form>
-        <label htmlFor=''>Products:</label>
-        {selectProducts(products)}
+        <label htmlFor='select-product'>Products:   </label>
+        <input id='select-product'>{selectProducts(products)}</input>
         <button type="submit">Add To Cart</button>
       </form>
+      <div className='cart-items'>
+        <ul>
+          <li>{cart.quantity === 1 ? cart.itemName : `${cart.itemName} x${cart.quantity}`}</li>
+        </ul>
+      </div>
       <p>
-        {itemCount === 0 ? "Add Some Items" : "You have " + {itemCount} +  " item(s) in your cart"}
+        {itemCount === 0 ? "Add Some Items" : `You have ${itemCount} item(s) in your cart`}
       </p>
     </>
   )
